1ff654b0d3e11b11ec21e56efef6fdbb667773e8
Dan Vera <arubinofaux@gmail.com>
2015-10-25 17:10:50 -0400

    show artist image

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  bio        :text
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Artist < ActiveRecord::Base
+  mount_uploader :image, AvatarUploader
 end
